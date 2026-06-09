b688525da6b87c93a64d66210a8dcf29db4be84b
llcardos <137669141+llcardos@users.noreply.github.com>
2026-06-09 19:13:25 -0300

    aula4

--- aula4/src/App.jsx
+++ aula4/src/App.jsx
@@ -1,122 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
+const url = "https://api.serratec.mwmsoftware.com/aliens"
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [aliens, setAliens] = useState([])
+
+  async function buscarAliens() {
+    try {
+      const response = await axios.get(url)
+      setAliens(response.data)
+    } catch (error) {
+      console.error('Error axios data:', error)
+    }
+  }
+
+  async function buscarAliensFetch() {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        setAliens(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchData()
+  }
+
+  useEffect(() => {
+    // buscarAliens()
+    buscarAliensFetch()
+  }, [])
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <h1>Lista de Aliens</h1>
+      {aliens.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        <div className="alien-list">
+          {aliens.map(alien => (
+            <p key={alien.id}>
+              <strong>{alien?.nome === 'string' ? 'Nome não disponível' : alien.nome}</strong> — {alien?.especie === 'string' ? 'Espécie não disponível' : alien.especie} (Periculosidade: {alien.periculosidade})
+              (Criado em: {alien.criadoEm}) - {alien.planeta} - (Descrição: {alien?.descricao === 'string' ? 'Descrição não disponível' : alien.descricao})
+            </p>
+          ))}
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      )}
     </>
   )
 }
 
 export default App
+
+// 0
+// : 
+// criadoEm
+// : 
+// "2026-06-03T19:12:27.301Z"
+// descricao
+// : 
+// "string"
+// especie
+// : 
+// "string"
+// id
+// : 
+// 2
+// nome
+// : 
+// "string"
+// periculosidade
+// : 
+// 10
+// planeta
+// : 
+// "string"
